a92326faa4c04261330715c1c96011b9a159ceff
NikitaS <dekgold@gmail.com>
2025-01-21 11:23:13 +0300

    Правки по вёрстке - 4 часть

--- app/src/main/java/ru/practicum/android/diploma/features/team/presentation/ui/MemberAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/team/presentation/ui/MemberAdapter.kt
@@ -6,15 +6,13 @@ import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.R
 
 class MemberAdapter(private val members: List<String>) : RecyclerView.Adapter<MemberViewHolder>() {
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.fragment_team_item, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_team_info, parent, false)
         return MemberViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MemberViewHolder, position: Int) {
-        val memberName = members[position]
-        holder.memberNameTextView.text = memberName
+        holder.memberNameTextView.text = members[position]
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/ru/practicum/android/diploma/features/team/presentation/ui/TeamInfoFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/team/presentation/ui/TeamInfoFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentTeamInfoBinding
 
@@ -14,8 +13,6 @@ class TeamInfoFragment : Fragment() {
 
     private var _binding: FragmentTeamInfoBinding? = null
     private val binding get() = _binding!!
-    private var recyclerView: RecyclerView? = null
-    private var adapter: MemberAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,27 +20,28 @@ class TeamInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTeamInfoBinding.inflate(inflater, container, false)
-        val view = binding.root
+        return binding.root
+    }
 
-        recyclerView = binding.teamRecyclerView
-        adapter = MemberAdapter(
-            listOf(
-                requireContext().getString(R.string.dev_name1),
-                requireContext().getString(R.string.dev_name2),
-                requireContext().getString(R.string.dev_name3),
-                requireContext().getString(R.string.dev_name4),
-                requireContext().getString(R.string.dev_name5)
-            )
-        )
-        recyclerView?.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView?.adapter = adapter
-        return view
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.teamRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.teamRecyclerView.adapter = MemberAdapter(getMembersList())
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        recyclerView = null
-        adapter = null
+    }
+
+    private fun getMembersList(): List<String> {
+        return listOf(
+            requireContext().getString(R.string.dev_name1),
+            requireContext().getString(R.string.dev_name2),
+            requireContext().getString(R.string.dev_name3),
+            requireContext().getString(R.string.dev_name4),
+            requireContext().getString(R.string.dev_name5)
+        )
     }
 }
