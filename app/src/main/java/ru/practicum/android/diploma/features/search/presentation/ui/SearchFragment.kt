f62e6e1c08395db316116d66e4ad4124b462a993
ИÐIgor Starkov <Kazesteam@yandex.ru>
2025-01-22 08:16:49 +0300

    HH-23: Реализует наследование фрагмента от общего

--- app/src/main/java/ru/practicum/android/diploma/features/search/presentation/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/search/presentation/ui/SearchFragment.kt
@@ -4,24 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
+import ru.practicum.android.diploma.features.common.presentation.ui.BaseFragment
 
-class SearchFragment : Fragment() {
+class SearchFragment : BaseFragment<FragmentSearchBinding>() {
 
-    private var binding: FragmentSearchBinding? = null
+    override fun createViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentSearchBinding {
+        viewBinding = FragmentSearchBinding.inflate(inflater, container, false)
+        return viewBinding
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentSearchBinding.inflate(inflater, container, false)
-        return binding?.root
+    ): View {
+        return createViewBinding(inflater, container).root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
+    override fun initUi() {
+
     }
+
+    override fun observeData() {
+
+    }
+
 }
