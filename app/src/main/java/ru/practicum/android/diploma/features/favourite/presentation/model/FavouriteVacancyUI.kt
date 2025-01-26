6f283b6060c4e4188855e661270eaa7a042ff7ba
ShevaVarya <shevchik.varvara@mail.ru>
2025-01-26 18:52:54 +0200

    HH-30: создает класс для UI

---
+++ app/src/main/java/ru/practicum/android/diploma/features/favourite/presentation/model/FavouriteVacancyUI.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.features.favourite.presentation.model
+
+data class FavouriteVacancyUI (
+    val title: String,
+    val salary: String,
+    val employerLogoUrl: String,
+    val employerName: String,
+    val location: String,
+)
