f493bf2d7149bc454a7c650c9cec84e1cb31903d
ИÐIgor Starkov <Kazesteam@yandex.ru>
2025-01-27 13:32:01 +0300

    HH-58: Побарывает datakt

--- app/src/main/java/ru/practicum/android/diploma/features/search/presentation/model/VacancySearchUIMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/search/presentation/model/VacancySearchUIMapper.kt
@@ -8,6 +8,8 @@ import java.text.NumberFormat
 import java.util.Locale
 
 private const val EMPTY_STRING = ""
+private const val SALARY_FORMAT = "%s %s"
+private const val DOUBLE_SALARY_FORMAT = "%s %s %s %s"
 
 fun Vacancy.toUI(resourceProvider: ResourceProvider): VacancySearchUI {
     return VacancySearchUI(
@@ -31,22 +33,24 @@ fun Vacancies.toUI(resourceProvider: ResourceProvider): VacanciesSearchUI {
 }
 
 private fun getProfession(vacancyName: String, city: String): String {
-    return String.format(Locale.getDefault(), "%s, %s", vacancyName, city)
+    return String.format(Locale.getDefault(), SALARY_FORMAT, vacancyName, city)
 }
 
 private fun getFormatSalary(vacancy: Vacancy, resourceProvider: ResourceProvider): String {
     val numberFormat = NumberFormat.getInstance(Locale.getDefault())
 
     val salaryFrom =
-        vacancy.salaryFrom.takeIf { it != 0 }?.let { "%s %s".format(numberFormat.format(it), vacancy.currencySymbol) }
+        vacancy.salaryFrom.takeIf { it != 0 }
+            ?.let { SALARY_FORMAT.format(numberFormat.format(it), vacancy.currencySymbol) }
             ?: EMPTY_STRING
     val salaryTo =
-        vacancy.salaryTo.takeIf { it != 0 }?.let { "%s %s".format(numberFormat.format(it), vacancy.currencySymbol) }
+        vacancy.salaryTo.takeIf { it != 0 }
+            ?.let { SALARY_FORMAT.format(numberFormat.format(it), vacancy.currencySymbol) }
             ?: EMPTY_STRING
 
     return when {
         salaryFrom.isNotEmpty() && salaryTo.isNotEmpty() ->
-            "%s %s %s %s".format(
+            DOUBLE_SALARY_FORMAT.format(
                 Locale.getDefault(),
                 resourceProvider.getString(R.string.salary_from),
                 salaryFrom,
@@ -55,10 +59,10 @@ private fun getFormatSalary(vacancy: Vacancy, resourceProvider: ResourceProvider
             )
 
         salaryFrom.isNotEmpty() ->
-            "%s %s".format(Locale.getDefault(), resourceProvider.getString(R.string.salary_from), salaryFrom)
+            SALARY_FORMAT.format(Locale.getDefault(), resourceProvider.getString(R.string.salary_from), salaryFrom)
 
         salaryTo.isNotEmpty() ->
-            "%s %s".format(Locale.getDefault(), resourceProvider.getString(R.string.salary_to), salaryTo)
+            SALARY_FORMAT.format(Locale.getDefault(), resourceProvider.getString(R.string.salary_to), salaryTo)
 
         else -> resourceProvider.getString(R.string.no_salary)
     }
