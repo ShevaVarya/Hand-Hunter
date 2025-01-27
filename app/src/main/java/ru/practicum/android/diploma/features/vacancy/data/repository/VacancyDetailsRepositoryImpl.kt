2022de2710a76da343945107df175c3d3e35aa38
ShevaVarya <shevchik.varvara@mail.ru>
2025-01-27 16:02:49 +0200

    HH-30: ипсравляет merge conflict

--- app/src/main/java/ru/practicum/android/diploma/features/vacancy/data/repository/VacancyDetailsRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/vacancy/data/repository/VacancyDetailsRepositoryImpl.kt
@@ -3,10 +3,9 @@ package ru.practicum.android.diploma.features.vacancy.data.repository
 import ru.practicum.android.diploma.features.common.data.database.AppDatabase
 import ru.practicum.android.diploma.features.common.data.network.service.NetworkClient
 import ru.practicum.android.diploma.features.common.domain.model.VacancyDetails
+import ru.practicum.android.diploma.features.favourite.data.dto.toDomain
 import ru.practicum.android.diploma.features.vacancy.data.dto.toDomain
 import ru.practicum.android.diploma.features.vacancy.domain.api.VacancyDetailsRepository
-import java.io.IOException
-import kotlin.coroutines.cancellation.CancellationException
 
 class VacancyDetailsRepositoryImpl(
     private val networkClient: NetworkClient,
@@ -14,30 +13,11 @@ class VacancyDetailsRepositoryImpl(
 ) : VacancyDetailsRepository {
 
     override suspend fun getVacancyDetails(vacancyId: String): Result<VacancyDetails> {
-        return runCatching {
-            networkClient.getVacancyById(vacancyId, mapOf()).map { it.toDomain() }
-        }.recoverCatching {
-            resolveError(it)
-        }
+        return networkClient.getVacancyById(vacancyId, mapOf()).map { it.toDomain() }
     }
 
     override suspend fun getFavouriteVacancy(vacancyId: String): VacancyDetails {
         val pair = appDatabase.favouritesDao().getFavouriteVacancy(vacancyId)
         return pair.first.toDomain(pair.second)
-    }
-
-    private fun resolveError(error: Throwable): Nothing {
-        throw when (error) {
-            is CancellationException -> error
-
-            is IOException -> CustomException.IOException
-
-            else -> CustomException.EmptyException
-        }
-    }
-
-    sealed class CustomException : Exception() {
-        object EmptyException : CustomException()
-        object IOException : CustomException()
     }
 }
