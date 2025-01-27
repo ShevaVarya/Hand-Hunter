483015e9e2b3852a440ebf10159c7a19d88f3e2c
ShevaVarya <shevchik.varvara@mail.ru>
2025-01-27 14:46:19 +0200

    HH-30: изменяет Mapper у Search и Favourite

--- app/src/main/java/ru/practicum/android/diploma/features/favourite/presentation/model/FavouriteVacancyUIMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/favourite/presentation/model/FavouriteVacancyUIMapper.kt
@@ -3,14 +3,19 @@ package ru.practicum.android.diploma.features.favourite.presentation.model
 import ru.practicum.android.diploma.features.common.domain.model.VacancyDetails
 import ru.practicum.android.diploma.features.common.presentation.ResourceProvider
 import ru.practicum.android.diploma.features.search.presentation.model.VacancySearchUI
-import ru.practicum.android.diploma.features.search.presentation.model.getProfession
-import ru.practicum.android.diploma.features.vacancy.presentation.model.toUI
+import ru.practicum.android.diploma.utils.getFormatSalary
+import ru.practicum.android.diploma.utils.getProfession
 
 fun VacancyDetails.toUi(resourceProvider: ResourceProvider): VacancySearchUI {
     return VacancySearchUI(
         id = id,
         formatedProfession = getProfession(title, location),
-        formatedSalary = salary.toUI(resourceProvider),
+        formatedSalary = getFormatSalary(
+            salary.from,
+            salary.to,
+            salary.currency.symbol,
+            resourceProvider
+        ),
         employerLogoUrl = employer.logoUrl ?: "",
         employerName = employer.name,
     )
---
+++ app/src/main/java/ru/practicum/android/diploma/utils/Formatter.kt
@@ -0,0 +1,52 @@
+package ru.practicum.android.diploma.utils
+
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.features.common.presentation.ResourceProvider
+import java.text.NumberFormat
+import java.util.Locale
+
+private const val EMPTY_STRING = ""
+private const val SALARY_FORMAT = "%s %s"
+private const val DOUBLE_SALARY_FORMAT = "%s %s %s %s"
+
+
+fun getProfession(vacancyName: String, city: String): String {
+    return String.format(Locale.getDefault(), SALARY_FORMAT, vacancyName, city)
+}
+
+fun getFormatSalary(
+    vacancySalaryFrom: Int,
+    vacancySalaryTo: Int,
+    vacancyCurrencySymbol: String,
+    resourceProvider: ResourceProvider
+): String {
+    val numberFormat = NumberFormat.getInstance(Locale.getDefault())
+
+    val salaryFrom =
+        vacancySalaryFrom.takeIf { it != 0 }
+            ?.let { SALARY_FORMAT.format(numberFormat.format(it), vacancyCurrencySymbol) }
+            ?: EMPTY_STRING
+    val salaryTo =
+        vacancySalaryTo.takeIf { it != 0 }
+            ?.let { SALARY_FORMAT.format(numberFormat.format(it), vacancyCurrencySymbol) }
+            ?: EMPTY_STRING
+
+    return when {
+        salaryFrom.isNotEmpty() && salaryTo.isNotEmpty() ->
+            DOUBLE_SALARY_FORMAT.format(
+                Locale.getDefault(),
+                resourceProvider.getString(R.string.salary_from),
+                salaryFrom,
+                resourceProvider.getString(R.string.salary_to),
+                salaryTo
+            )
+
+        salaryFrom.isNotEmpty() ->
+            SALARY_FORMAT.format(Locale.getDefault(), resourceProvider.getString(R.string.salary_from), salaryFrom)
+
+        salaryTo.isNotEmpty() ->
+            SALARY_FORMAT.format(Locale.getDefault(), resourceProvider.getString(R.string.salary_to), salaryTo)
+
+        else -> resourceProvider.getString(R.string.no_salary)
+    }
+}
