e848a75b948fb137b4abd2a6233a3f06b6d7a755
ИÐIgor Starkov <Kazesteam@yandex.ru>
2025-01-27 13:48:00 +0300

    HH-58: Убирает маппинг из интерактора во втюмодель

--- app/src/main/java/ru/practicum/android/diploma/features/search/domain/interactor/VacanciesSearchInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/search/domain/interactor/VacanciesSearchInteractor.kt
@@ -1,8 +1,9 @@
 package ru.practicum.android.diploma.features.search.domain.interactor
 
 import ru.practicum.android.diploma.features.search.domain.model.QuerySearch
+import ru.practicum.android.diploma.features.search.domain.model.Vacancies
 import ru.practicum.android.diploma.features.search.presentation.model.VacanciesSearchUI
 
 interface VacanciesSearchInteractor {
-    suspend fun getVacancies(querySearch: QuerySearch): Result<VacanciesSearchUI>
+    suspend fun getVacancies(querySearch: QuerySearch): Result<Vacancies>
 }
--- app/src/main/java/ru/practicum/android/diploma/features/search/domain/interactor/VacanciesSearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/search/domain/interactor/VacanciesSearchInteractorImpl.kt
@@ -1,17 +1,14 @@
 package ru.practicum.android.diploma.features.search.domain.interactor
 
-import ru.practicum.android.diploma.features.common.presentation.ResourceProvider
 import ru.practicum.android.diploma.features.search.domain.api.VacanciesSearchRepository
 import ru.practicum.android.diploma.features.search.domain.model.QuerySearch
-import ru.practicum.android.diploma.features.search.presentation.model.VacanciesSearchUI
-import ru.practicum.android.diploma.features.search.presentation.model.toUI
+import ru.practicum.android.diploma.features.search.domain.model.Vacancies
 
 class VacanciesSearchInteractorImpl(
     private val repository: VacanciesSearchRepository,
-    private val resourceProvider: ResourceProvider
 ) : VacanciesSearchInteractor {
-    override suspend fun getVacancies(querySearch: QuerySearch): Result<VacanciesSearchUI> {
-        return repository.searchVacancies(querySearch).mapCatching { it.toUI(resourceProvider) }
+    override suspend fun getVacancies(querySearch: QuerySearch): Result<Vacancies> {
+        return repository.searchVacancies(querySearch).mapCatching { it }
     }
 
 }
