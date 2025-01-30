130003a5140bd31ebe012723e85515fb99dc2dbc
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-30 20:28:32 +0300

    HH-65: Создать модель Filter.kt

---
+++ app/src/main/java/ru/practicum/android/diploma/features/filters/domain/model/Filter.kt
@@ -0,0 +1,21 @@
+package ru.practicum.android.diploma.features.filters.domain.model
+
+/**
+ * @param country `Nullable` Хранит экземпляр класса `CountryUI` с информацией о стране
+ * @param region `Nullable` Хранит экземпляр класса `RegionUI` с информацией о регионе
+ * @param industry `Nullable` Хранит экземпляр класса `IndustryUI` с информацией о регионе
+ * @param salary `Nullable` Хранит информацию об уровне зарплаты
+ * @param onlyWithSalary  Хранит флаг, указывающий на необходимость игнорирования вакансий без указания зарплаты
+ * @property isDefault  Хранит флаг, указывающий на то, являются ли значения фильтра значениями по умолчанию
+ */
+
+data class Filter(
+    //val country: CountryUI? = null,
+    //val region: RegionUI? = null,
+    //val industry: IndustryUI? = null,
+    val salary: Int? = null,
+    val onlyWithSalary: Boolean = false
+) {
+//    val isDefault: Boolean
+//        get() = country == null && region == null && industry == null && !onlyWithSalary && salary == null
+}
