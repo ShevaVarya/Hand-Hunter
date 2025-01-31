7cea95e09c5fb377e1d3076edcf47b196674c75d
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-31 16:55:33 +0300

    HH-65: Добавить зависимость SpecializationSelectionViewModel в ViewModelModule.kt

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -6,6 +6,7 @@ import ru.practicum.android.diploma.features.favourite.presentation.viewmodel.Fa
 import ru.practicum.android.diploma.features.filters.presentation.viewmodel.SearchFilterViewModel
 import ru.practicum.android.diploma.features.search.presentation.viewmodel.SearchViewModel
 import ru.practicum.android.diploma.features.selectlocation.presentation.viewmodel.LocationSelectionViewModel
+import ru.practicum.android.diploma.features.selectspecialization.presentation.viewmodel.SpecializationSelectionViewModel
 import ru.practicum.android.diploma.features.selectworkplace.presentation.viewmodel.WorkplaceSelectionViewModel
 import ru.practicum.android.diploma.features.vacancy.presentation.viewmodel.VacancyInfoViewModel
 
@@ -17,4 +18,5 @@ val viewModelModule = module {
     viewModelOf(::LocationSelectionViewModel)
     viewModelOf(::WorkplaceSelectionViewModel)
     viewModelOf(::SearchFilterViewModel)
+    viewModelOf(::SpecializationSelectionViewModel)
 }
