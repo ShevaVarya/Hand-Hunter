2dd9f9c6a0aa6dd834ad89d4f05f6faaa8ac55e7
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-01-31 15:52:44 +0300

    HH-65: Добавить зависимость SearchFiltersViewModel в ViewModelModule.kt

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.di
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import ru.practicum.android.diploma.features.favourite.presentation.viewmodel.FavouriteVacanciesViewModel
+import ru.practicum.android.diploma.features.filters.presentation.viewmodel.SearchFilterViewModel
 import ru.practicum.android.diploma.features.search.presentation.viewmodel.SearchViewModel
 import ru.practicum.android.diploma.features.selectlocation.presentation.viewmodel.LocationSelectionViewModel
 import ru.practicum.android.diploma.features.selectworkplace.presentation.viewmodel.WorkplaceSelectionViewModel
@@ -15,4 +16,5 @@ val viewModelModule = module {
     viewModelOf(::SearchViewModel)
     viewModelOf(::LocationSelectionViewModel)
     viewModelOf(::WorkplaceSelectionViewModel)
+    viewModelOf(::SearchFilterViewModel)
 }
