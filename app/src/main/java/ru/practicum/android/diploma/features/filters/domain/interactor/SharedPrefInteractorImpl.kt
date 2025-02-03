061d8ded768e4f4407419200b892b3534ae88dbc
KillReal34 <85308586+KillReal34@users.noreply.github.com>
2025-02-04 01:08:57 +0300

    HH-73: Удалить неиспользуемые методы в SharedPrefInteractorImpl.kt

--- app/src/main/java/ru/practicum/android/diploma/features/filters/domain/interactor/SharedPrefInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/filters/domain/interactor/SharedPrefInteractorImpl.kt
@@ -1,9 +1,6 @@
 package ru.practicum.android.diploma.features.filters.domain.interactor
 
-import ru.practicum.android.diploma.features.common.domain.filter.model.FilterCountry
-import ru.practicum.android.diploma.features.common.domain.filter.model.FilterIndustry
 import ru.practicum.android.diploma.features.common.domain.filter.model.FilterMainData
-import ru.practicum.android.diploma.features.common.domain.filter.model.FilterRegion
 import ru.practicum.android.diploma.features.common.domain.filter.repositoryapi.FilterRepository
 import ru.practicum.android.diploma.features.filters.domain.api.SharedPrefInteractor
 
@@ -14,20 +11,8 @@ class SharedPrefInteractorImpl(
         repository.setSalary(salary)
     }
 
-    override fun saveIndustry(industry: FilterIndustry) {
-        repository.setIndustry(industry)
-    }
-
     override fun saveWithoutSalary(check: Boolean) {
         repository.setIsNeedToHideVacancyWithoutSalary(check)
-    }
-
-    override fun saveCountry(country: FilterCountry) {
-        repository.setCountry(country)
-    }
-
-    override fun saveRegion(region: FilterRegion) {
-        repository.setRegion(region)
     }
 
     override fun loadFilter(): FilterMainData {
