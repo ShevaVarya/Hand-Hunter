f99f5b7a641d926ac3ad19385468717cf748bb7c
ShevaVarya <shevchik.varvara@mail.ru>
2025-02-03 15:42:37 +0200

    HH-68: добавляет в интерактор получение данных из префы

--- app/src/main/java/ru/practicum/android/diploma/features/search/domain/interactor/VacanciesSearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/search/domain/interactor/VacanciesSearchInteractorImpl.kt
@@ -1,14 +1,21 @@
 package ru.practicum.android.diploma.features.search.domain.interactor
 
+import ru.practicum.android.diploma.features.common.domain.filter.model.FilterMainData
+import ru.practicum.android.diploma.features.common.domain.filter.repositoryapi.FilterRepository
 import ru.practicum.android.diploma.features.search.domain.api.VacanciesSearchRepository
 import ru.practicum.android.diploma.features.search.domain.model.QuerySearch
 import ru.practicum.android.diploma.features.search.domain.model.Vacancies
 
 class VacanciesSearchInteractorImpl(
     private val repository: VacanciesSearchRepository,
+    private val filterRepository: FilterRepository
 ) : VacanciesSearchInteractor {
     override suspend fun getVacancies(querySearch: QuerySearch): Result<Vacancies> {
         return repository.searchVacancies(querySearch).mapCatching { it }
+    }
+
+    override fun getFilters(): FilterMainData {
+        return filterRepository.getFilterMainData()
     }
 
 }
