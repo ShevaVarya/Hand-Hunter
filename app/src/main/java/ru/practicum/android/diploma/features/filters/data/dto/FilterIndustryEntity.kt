2f33b49e7289c55bb2676490a0181235493f553d
NSPROJ <dekgold@gmail.com>
2025-02-07 23:56:55 +0300

    HH-161: Экран "Выбор отрасли" - не сохраняет своё состояние

--- app/src/main/java/ru/practicum/android/diploma/features/filters/data/dto/FilterIndustryEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/filters/data/dto/FilterIndustryEntity.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.features.filters.data.dto
 
 import ru.practicum.android.diploma.features.filters.domain.model.Industry
 
-@Suppress("DetektRuleName")
+@Suppress("DataClassContainsFunctions")
 data class FilterIndustryEntity(
     val id: String?,
     val name: String?
