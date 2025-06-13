85148d37da02b10b8e9d3fd89b89652eb7bcd62e
ShevaVarya <shevchik.varvara@mail.ru>
2025-06-13 15:20:47 +0300

    screenshots

--- app/src/main/java/ru/practicum/android/diploma/features/favourite/presentation/ui/FavouriteVacanciesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/favourite/presentation/ui/FavouriteVacanciesFragment.kt
@@ -1,6 +1,9 @@
 package ru.practicum.android.diploma.features.favourite.presentation.ui
 
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
